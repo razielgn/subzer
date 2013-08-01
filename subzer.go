2779f2016873512ecfbe3d3282703a6f30a151cd
Federico Ravasio <ravasio.federico@gmail.com>
2013-08-01 11:28:37 +0200

    Specs and implementation on timestamp and duration functions.

--- subzer.go
+++ subzer.go
@@ -1,7 +1,27 @@
 package main
 
-func truth() bool {
-    return true
+import (
+    "regexp"
+    "strconv"
+    "time"
+)
+
+func ReduceDuration(start time.Duration, end time.Duration) int {
+    return int(end.Seconds() - start.Seconds())
+}
+
+func ParseTimestamp(timestamp string) time.Duration {
+    pieces := regexp.MustCompile(":|,").Split(timestamp, 4)
+
+    hours, _ := strconv.ParseUint(pieces[0], 10, 8)
+    minutes, _ := strconv.ParseUint(pieces[1], 10, 8)
+    seconds, _ := strconv.ParseUint(pieces[2], 10, 8)
+    milliseconds, _ := strconv.ParseUint(pieces[3], 10, 16)
+
+    return time.Duration(hours)*time.Hour +
+        time.Duration(minutes)*time.Minute +
+        time.Duration(seconds)*time.Second +
+        time.Duration(milliseconds)*time.Millisecond
 }
 
 func main() {
--- subzer_test.go
+++ subzer_test.go
@@ -1,9 +1,46 @@
 package main
 
-import "testing"
+import (
+    "testing"
+    "time"
+)
 
-func TestTruth(t *testing.T) {
-    if !truth() {
-        t.Error("Expected the truth, liar.")
+func TestParseTimestamp1(t *testing.T) {
+    timestamp := "00:00:00,000"
+    expected_duration := 0 * time.Second
+    duration := ParseTimestamp(timestamp)
+
+    if duration != expected_duration {
+        t.Errorf("Expected duration to equal %s, got %s", expected_duration, duration)
+    }
+}
+
+func TestParseTimestamp2(t *testing.T) {
+    timestamp := "08:17:20,301"
+    expected_duration := 8*time.Hour + 17*time.Minute + 20*time.Second + 301*time.Millisecond
+    duration := ParseTimestamp(timestamp)
+
+    if duration != expected_duration {
+        t.Errorf("Expected duration to equal %s, got %s", expected_duration, duration)
+    }
+}
+
+func TestReduceDurations1(t *testing.T) {
+    start := ParseTimestamp("00:00:02,110")
+    end := ParseTimestamp("00:00:04,450")
+
+    seconds := ReduceDuration(start, end)
+    if seconds != 2 {
+        t.Errorf("Expected seconds to equal 2, got %d", seconds)
+    }
+}
+
+func TestReduceDurations2(t *testing.T) {
+    start := ParseTimestamp("00:00:00,110")
+    end := ParseTimestamp("00:00:08,950")
+
+    seconds := ReduceDuration(start, end)
+    if seconds != 8 {
+        t.Errorf("Expected seconds to equal 8, got %d", seconds)
     }
 }
