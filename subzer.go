99b58b9409ac43005c27d3981c378f0cdc356798
Federico Ravasio <ravasio.federico@gmail.com>
2013-08-02 11:51:30 +0200

    Final implementation.

--- subzer.go
+++ subzer.go
@@ -1,13 +1,69 @@
 package main
 
 import (
+    "bufio"
+    "container/list"
+    "fmt"
+    flags "github.com/jessevdk/go-flags"
+    "io"
+    "os"
+    "path/filepath"
     "regexp"
     "strconv"
+    "strings"
     "time"
 )
 
-func ReduceDuration(start time.Duration, end time.Duration) int {
-    return int(end.Seconds() - start.Seconds())
+type SrtBlock struct {
+    number uint64
+    start  time.Duration
+    end    time.Duration
+    text   []string
+}
+
+func NewSrtBlock(number uint64, start, end time.Duration, text []string) *SrtBlock {
+    b := SrtBlock{number, start, end, text}
+    return &b
+}
+
+func (s *SrtBlock) String() string {
+    return fmt.Sprintf("%#v", s)
+}
+
+func (s *SrtBlock) Text() []string {
+    return s.text
+}
+
+func (s *SrtBlock) TextAsLine() string {
+    return strings.Join(s.Text(), " ")
+}
+
+func (s *SrtBlock) Duration() uint64 {
+    return ReduceDuration(s.start, s.end)
+}
+
+func SrtBlockParse(lines []string) *SrtBlock {
+    if len(lines) < 3 {
+        panic("Block should have at least 3 lines.")
+    }
+
+    number, _ := strconv.ParseUint(lines[0], 10, 64)
+
+    durations := strings.Split(lines[1], " --> ")
+    if len(durations) != 2 {
+        panic("Block should have two timestamps.")
+    }
+
+    start := ParseTimestamp(durations[0])
+    end := ParseTimestamp(durations[1])
+
+    text := lines[2:]
+
+    return NewSrtBlock(number, start, end, text)
+}
+
+func ReduceDuration(start time.Duration, end time.Duration) uint64 {
+    return uint64(end.Seconds()) - uint64(start.Seconds())
 }
 
 func ParseTimestamp(timestamp string) time.Duration {
@@ -24,5 +80,157 @@ func ParseTimestamp(timestamp string) time.Duration {
         time.Duration(milliseconds)*time.Millisecond
 }
 
+func ListToSlice(list *list.List) []string {
+    slice := make([]string, list.Len())
+
+    i := 0
+    for e := list.Front(); e != nil; e = e.Next() {
+        slice[i] = e.Value.(string)
+        i++
+    }
+
+    return slice
+}
+
+func ParseSrtStream(source io.Reader) *list.List {
+    reader := bufio.NewScanner(source)
+    blocks := list.New()
+
+    for {
+        block_lines := list.New()
+
+        for reader.Scan() {
+            line := reader.Text()
+
+            if line == "" {
+                break
+            }
+
+            block_lines.PushBack(line)
+        }
+
+        if block_lines.Len() == 0 {
+            break
+        }
+
+        srt_block := SrtBlockParse(ListToSlice(block_lines))
+        blocks.PushBack(srt_block)
+    }
+
+    return blocks
+}
+
+func WriteFileLine(writer io.Writer, seconds uint64, text string) {
+    line := fmt.Sprintf("%06d\t%s\n", seconds, text)
+    io.WriteString(writer, line)
+}
+
+func ConvertSrtStream(filename string, source io.Reader, writer io.Writer) uint64 {
+    blocks := ParseSrtStream(source)
+
+    WriteFileLine(writer, 0, filename)
+
+    var prev_seconds uint64 = 0
+    var curr_seconds uint64 = 0
+
+    for e := blocks.Front(); e != nil; e = e.Next() {
+        block := e.Value.(*SrtBlock)
+
+        WriteFileLine(writer, prev_seconds, block.TextAsLine())
+
+        curr_seconds = block.Duration()
+        prev_seconds += curr_seconds
+    }
+
+    return prev_seconds
+}
+
+func WriteTotalSeconds(seeker io.Seeker, writer io.Writer, seconds uint64) {
+    io.WriteString(writer, fmt.Sprintf("%06d", seconds))
+}
+
+func StreamConversion(destination_name string, source io.Reader, writer io.Writer, seeker io.Seeker) {
+    seconds := ConvertSrtStream(destination_name, source, writer)
+
+    seeker.Seek(0, 0)
+    WriteFileLine(writer, seconds, destination_name)
+}
+
+func ProcessFile(source_path string) {
+    destination_path := regexp.MustCompile("srt$").ReplaceAllString(source_path, "txt")
+    destination_name := filepath.Base(destination_path)
+
+    source, err := os.Open(source_path)
+    if err != nil {
+        panic("Cannot open source file.")
+    }
+    defer source.Close()
+
+    destination, err := os.Create(destination_path)
+    if err != nil {
+        panic("Cannot open source file.")
+    }
+    defer destination.Close()
+
+    StreamConversion(destination_name, source, destination, destination)
+}
+
 func main() {
+    var opts struct {
+        File      string `short:"i" description:"File input"`
+        Directory string `short:"r" description:"Folder input"`
+    }
+
+    parser := flags.NewParser(&opts, flags.Default)
+    parser.ApplicationName = "subzer"
+    parser.Usage = "[OPTIONS]"
+
+    _, err := parser.Parse()
+
+    if err != nil {
+        os.Exit(1)
+    }
+
+    file := opts.File
+    dir := opts.Directory
+
+    if len(file) > 0 && len(dir) > 0 {
+        fmt.Println("Error: input a file or a directory, not both.")
+        os.Exit(1)
+    }
+
+    if len(file) == 0 && len(dir) == 0 {
+        parser.WriteHelp(os.Stdout)
+        os.Exit(1)
+    }
+
+    files := list.New()
+
+    if len(file) > 0 {
+        if _, err := os.Stat(file); err != nil {
+            fmt.Printf("Error: file \"%s\" does not exist.\n", file)
+            os.Exit(1)
+        }
+
+        files.PushBack(file)
+    }
+
+    if len(dir) > 0 {
+        if _, err := os.Stat(dir); err != nil {
+            fmt.Printf("Error: directory %s does not exist.\n", dir)
+            os.Exit(1)
+        }
+
+        err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+            if filepath.Ext(path) == ".srt" {
+                files.PushBack(path)
+            }
+
+            return err
+        })
+    }
+
+    for e := files.Front(); e != nil; e = e.Next() {
+        ProcessFile(e.Value.(string))
+    }
 }
--- subzer_test.go
+++ subzer_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    stringio "github.com/kdar/stringio"
     "testing"
     "time"
 )
@@ -26,8 +27,8 @@ func TestParseTimestamp2(t *testing.T) {
 }
 
 func TestReduceDurations1(t *testing.T) {
-    start := ParseTimestamp("00:00:02,110")
-    end := ParseTimestamp("00:00:04,450")
+    start := ParseTimestamp("00:00:07,390")
+    end := ParseTimestamp("00:00:09,280")
 
     seconds := ReduceDuration(start, end)
     if seconds != 2 {
@@ -42,5 +43,94 @@ func TestReduceDurations2(t *testing.T) {
     seconds := ReduceDuration(start, end)
     if seconds != 8 {
         t.Errorf("Expected seconds to equal 8, got %d", seconds)
+    }
+}
+
+func TestSrtBlockEquality(t *testing.T) {
+    start := (0 * time.Second)
+    end := (2*time.Second + 110*time.Millisecond)
+    text := []string{"[Salim Ismail] [Global Ambassador, Singularity University] Do you think you have what it takes"}
+    expected := NewSrtBlock(1, start, end, text)
+    output := NewSrtBlock(1, start, end, text)
+
+    if expected.String() != output.String() {
+        t.Errorf("Expected parsed srt block to be\n%#+v\ngot\n%#+v", expected, output)
+    }
+}
+
+func SrtBlockPieces() []string {
+    return []string{"1", "00:00:00,000 --> 00:00:02,110", "[Salim Ismail] [Global Ambassador, Singularity University] Do you think you have what it takes", "Whatever"}
+}
+
+func TestSrtBlockParsing1(t *testing.T) {
+    srt_block := SrtBlockPieces()
+
+    start := (0 * time.Second)
+    end := (2*time.Second + 110*time.Millisecond)
+    text := []string{"[Salim Ismail] [Global Ambassador, Singularity University] Do you think you have what it takes", "Whatever"}
+    expected := NewSrtBlock(1, start, end, text)
+    output := SrtBlockParse(srt_block)
+
+    if expected.String() != output.String() {
+        t.Errorf("Expected parsed srt block to be\n%#+v\ngot\n%#+v", expected, output)
+    }
+}
+
+func TestSrtBlockParsing2(t *testing.T) {
+    srt_block := SrtBlockPieces()
+
+    start := (0 * time.Second)
+    end := (2*time.Second + 110*time.Millisecond)
+    text := []string{"[Sxlim Ismail] [Global Ambassador, Singularity University] Do you think you have what it takes"}
+    expected := NewSrtBlock(1, start, end, text)
+    output := SrtBlockParse(srt_block)
+
+    if expected.String() == output.String() {
+        t.Errorf("Expected parsed srt block to be\n%#v\ngot\n%#v", expected, output)
+    }
+}
+
+func SourceStringExample() string {
+    return `1
+00:00:00,000 --> 00:00:02,110
+[Salim Ismail] [Global Ambassador, Singularity University] Do you think you have what it takes
+
+2
+00:00:02,110 --> 00:00:04,450
+to positively impact a billion people around the world?
+
+3
+00:00:04,450 --> 00:00:07,390
+Here are a few of our students that do.
+
+4
+00:00:07,390 --> 00:00:09,280
+[♪ Music ♪]
+
+`
+}
+
+func DestinationStringExample() string {
+    return "000009\twhatever.txt\n" +
+        "000000\t[Salim Ismail] [Global Ambassador, Singularity University] Do you think you have what it takes\n" +
+        "000002\tto positively impact a billion people around the world?\n" +
+        "000004\tHere are a few of our students that do.\n" +
+        "000007\t[♪ Music ♪]\n"
+}
+
+func TestConversion1(t *testing.T) {
+    source := stringio.New()
+    destination := stringio.New()
+
+    source.WriteString(SourceStringExample())
+    source.Seek(0, 0)
+
+    StreamConversion("whatever.txt", source, destination, destination)
+    output := destination.GetValueString()
+
+    expected_output := DestinationStringExample()
+
+    if expected_output != output {
+        t.Errorf("Expected output to be\n%s\ngot\n\n%s\n", expected_output, output)
     }
 }
